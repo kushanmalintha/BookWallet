a7f03ceb8db43ef0851ce71dd39aa774b1937027
RaviAlpha <ravindupathirage12345@gmail.com>
2024-07-01 13:07:35 +0530

    added dark theme colors as colors.dart

---
+++ book_wallert/lib/colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MyColors {
+  static const selectedItemColor = Color(0xFF91D786);
+  static const Color bgColor = Color(0xFF191919);
+  static const Color panelColor = Color(0xFF3A3939);
+  static const Color textColor = Color(0xFFF3EFEF);
+  static const Color text2Color = Color(0xFFC9D2C8);
+  static const Color nonSelectedItemColor = Color(0xFFB4BCB3);
+  static const Color navigationBarColor = Color(0xFF34362E);
+  static const Color titleColor = Color(0xFFFFFFFF);
+}
