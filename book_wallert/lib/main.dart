6bad826e121c17a1d327eb980a7a25ecc6542831
RaviAlpha <ravindupathirage12345@gmail.com>
2024-07-03 21:57:53 +0530

    added TestScreens

--- book_wallert/lib/main.dart
+++ book_wallert/lib/main.dart
@@ -1,5 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:book_wallert/screens/main_screen/main_screen_frame.dart';
+import 'package:book_wallert/screens/test_screens/test_screen_frame.dart';
+import 'package:book_wallert/screens/test_screens/screen1/screen1.dart';
+import 'package:book_wallert/screens/test_screens/screen2/screen2.dart';
+import 'package:book_wallert/screens/test_screens/screen3/screen3.dart';
+import 'package:book_wallert/screens/test_screens/screen4/screen4.dart';
+import 'package:book_wallert/screens/test_screens/screen5/screen5.dart';
+import 'package:book_wallert/screens/test_screens/screen6/screen6.dart';
+import 'package:book_wallert/screens/test_screens/screen7/screen7.dart';
+import 'package:book_wallert/screens/test_screens/screen8/screen8.dart';
 
 // The main entry point of the application.
 void main() => runApp(const MyApp());
@@ -9,10 +18,11 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
+      title: "BookWallet",
       // The main screen of the application.
-      home: MainScreen(),
+      // home: const MainScreen(),
 
       // ...If need to add new different screen, uncomment below and edit...
       //Navigator.pushReplacementNamed(context, '/Profile'); call this function replacing the name
@@ -22,6 +32,22 @@ class MyApp extends StatelessWidget {
       //   '/screenname1': (context) => ScreenName1(),
       //   '/Profile': (context) => const ScreenName2(),
       // },
+
+      initialRoute: '/MainScreen',
+
+      routes: {
+        // adding routes
+        '/MainScreen': (context) => const MainScreen(),
+        '/TestScreen': (context) => TestScreen(),
+        '/screen1': (context) => const Screen1(),
+        '/screen2': (context) => const Screen2(),
+        '/screen3': (context) => const Screen3(),
+        '/screen4': (context) => const Screen4(),
+        '/screen5': (context) => const Screen5(),
+        '/screen6': (context) => const Screen6(),
+        '/screen7': (context) => const Screen7(),
+        '/screen8': (context) => const Screen8(),
+      },
     );
   }
 }
