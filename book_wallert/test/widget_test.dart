48ec9e644c4ac166e8a961514934a91e8c79c824
Akash <e20100@eng.pdn.ac.lk>
2024-07-03 14:21:45 +0530

    done

--- book_wallert/test/widget_test.dart
+++ book_wallert/test/widget_test.dart
@@ -8,12 +8,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:book_wallert/main.dart';
-https://github.com/AIFERNANDOE20100/BookWallet.git
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(const MyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
