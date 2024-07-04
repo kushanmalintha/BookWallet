9afbcab0dfe31731f44f009675a69988b60c6a06
kushanmalintha <e20243@eng.pdn.ac.lk>
2024-07-04 16:10:01 +0530

    make review card circular edged, create login page on screen 1, create custom button and custom textbox

---
+++ book_wallert/lib/buttons/custom_button1.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+// A custom button widget with round edges
+class CustomToggleButton1 extends StatelessWidget {
+  // Text to be displayed on the button
+  final String text;
+
+  // Function to be called when the button is pressed
+  final Function press;
+
+  // Background color of the button
+  final Color backgroundColor;
+
+  // Text color of the button
+  final Color textColor;
+
+  // Border color of the button
+  final Color borderColor;
+
+  // Constructor to initialize the button properties
+  const CustomToggleButton1({
+    super.key,
+    required this.text,
+    required this.press,
+    required this.backgroundColor,
+    required this.textColor,
+    required this.borderColor,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      // Define the button's style
+      style: TextButton.styleFrom(
+        // Padding inside the button
+        padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 30),
+        // Background color of the button
+        backgroundColor: backgroundColor,
+        // border color of the button
+        side: BorderSide(color: borderColor),
+        // Text color of the button
+        foregroundColor: textColor,
+      ),
+      // Define the function to be called when the button is pressed
+      onPressed: () => press(),
+      // Define the child widget (text) of the button
+      child: Text(
+        text,
+        style: const TextStyle(
+          // Font size of the text
+          fontSize: 18,
+        ),
+      ),
+    );
+  }
+}
