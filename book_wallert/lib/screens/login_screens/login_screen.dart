ec5bef54b4b84d61557d4410fa71bd70d57ac5ee
RaviAlpha <ravindupathirage12345@gmail.com>
2024-07-14 13:40:13 +0530

    created initial login settedup

--- book_wallert/lib/screens/login_screens/login_screen.dart
+++ book_wallert/lib/screens/login_screens/login_screen.dart
@@ -14,82 +14,86 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: MyColors.bgColor,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const CircleAvatar(
-              backgroundImage: AssetImage("images/Book_Image1.jpg"),
-              radius: 60,
-            ),
-            const SizedBox(height: 10),
-            const Text(
-              "WELCOME TO BOOKWALLET",
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 25.0,
-                color: MyColors.textColor,
+    return PopScope(
+      canPop: false,
+      child: Scaffold(
+        backgroundColor: MyColors.bgColor,
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const CircleAvatar(
+                backgroundImage: AssetImage("images/Book_Image1.jpg"),
+                radius: 60,
               ),
-            ),
-            const SizedBox(height: 10),
-            CustomTextBox(
-              hintText: "Username",
-              type: TextInputType.text, // Adjusted for username
-              controller: _loginController.usernameController,
-            ),
-            const SizedBox(height: 10),
-            CustomTextBox(
-              hintText: "Password",
-              type: TextInputType.visiblePassword,
-              isPassword: true,
-              controller: _loginController.passwordController,
-            ),
-            const SizedBox(height: 10),
-            CustomButton1(
-              text: "Login",
-              press: () => _loginController.login(context),
-            ),
-            const SizedBox(height: 10),
-            GestureDetector(
-              onTap: () {
-                // Forgot password function
-                print("Forgot password");
-              },
-              child: const Text(
-                "I forgot my password",
-                style: TextStyle(color: MyColors.textColor, fontSize: 12),
-              ),
-            ),
-            const SizedBox(height: 10),
-            GestureDetector(
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => SignupScreen()),
-                );
-              },
-              child: RichText(
-                text: const TextSpan(
-                  children: [
-                    TextSpan(
-                      text: "Don't have an account? ",
-                      style: TextStyle(color: MyColors.textColor, fontSize: 12),
-                    ),
-                    TextSpan(
-                      text: "SIGN UP",
-                      style: TextStyle(
-                        color: Colors.blue,
-                        fontSize: 12,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ],
+              const SizedBox(height: 10),
+              const Text(
+                "WELCOME TO BOOKWALLET",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 25.0,
+                  color: MyColors.textColor,
                 ),
               ),
-            ),
-          ],
+              const SizedBox(height: 10),
+              CustomTextBox(
+                hintText: "Username",
+                type: TextInputType.text, // Adjusted for username
+                controller: _loginController.usernameController,
+              ),
+              const SizedBox(height: 10),
+              CustomTextBox(
+                hintText: "Password",
+                type: TextInputType.visiblePassword,
+                isPassword: true,
+                controller: _loginController.passwordController,
+              ),
+              const SizedBox(height: 10),
+              CustomButton1(
+                text: "Login",
+                press: () => _loginController.login(context),
+              ),
+              const SizedBox(height: 10),
+              GestureDetector(
+                onTap: () {
+                  // Forgot password function
+                  print("Forgot password");
+                },
+                child: const Text(
+                  "I forgot my password",
+                  style: TextStyle(color: MyColors.textColor, fontSize: 12),
+                ),
+              ),
+              const SizedBox(height: 10),
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => SignupScreen()),
+                  );
+                },
+                child: RichText(
+                  text: const TextSpan(
+                    children: [
+                      TextSpan(
+                        text: "Don't have an account? ",
+                        style:
+                            TextStyle(color: MyColors.textColor, fontSize: 12),
+                      ),
+                      TextSpan(
+                        text: "SIGN UP",
+                        style: TextStyle(
+                          color: Colors.blue,
+                          fontSize: 12,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
