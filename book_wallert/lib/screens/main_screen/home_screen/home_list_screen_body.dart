f4c3378b1324ed6f31c61d0b1c95b8bdac7f64cc
RaviAlpha <ravindupathirage12345@gmail.com>
2024-07-15 02:14:30 +0530

    created initial login settedup

--- book_wallert/lib/screens/main_screen/home_screen/home_list_screen_body.dart
+++ book_wallert/lib/screens/main_screen/home_screen/home_list_screen_body.dart
@@ -7,21 +7,6 @@ class HomeListScreenBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // List of review data
-    final List<Map<String, String>> reviews = [
-      {
-        'imagePath': 'images/Book_Image1.jpg',
-        'bookName': 'Dune Messiah',
-        'authorName': 'Frank Herbert',
-        'description':
-            "Dune Messiah continues the story of Paul Atredes, now Emperor of the Known Universe and known as Muad'Dib. As he grapples with immense power, political enemies, and a conspiracy within his own",
-        'rating': 'Rating : 8.9/10',
-        'reviewedBy': 'Reviewed By : ',
-        'userName': 'Ravindu Pathirage',
-      },
-      // Add more review maps here if needed
-    ];
-
     int reviewCount = 5;
 
     return SingleChildScrollView(
