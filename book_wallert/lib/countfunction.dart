66a0ee7047e09059156b66a71f60cf54c03d0617
kushanmalintha <e20243@eng.pdn.ac.lk>
2024-07-28 21:58:57 +0530

    fix like and share

--- book_wallert/lib/countfunction.dart
+++ book_wallert/lib/countfunction.dart
@@ -1,1 +1,14 @@
+class CountReact {
+  int likeCount;
+  int shareCount;
 
+  CountReact({this.likeCount = 100, this.shareCount = 100});
+
+  void likeIncrease() {
+    likeCount++;
+  }
+
+  void shareIncrease() {
+    shareCount++;
+  }
+}
