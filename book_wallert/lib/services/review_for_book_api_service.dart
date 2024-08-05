d9c304ad449232280986f6ec78d83d47137ce9f5
kushanmalintha <e20243@eng.pdn.ac.lk>
2024-08-05 14:11:03 +0530

    book recommend to followers

--- book_wallert/lib/services/review_for_book_api_service.dart
+++ book_wallert/lib/services/review_for_book_api_service.dart
@@ -1,6 +1,6 @@
 import 'dart:convert';
 import 'package:book_wallert/ipaddress.dart';
-import 'package:book_wallert/models/book_model.dart';
+// import 'package:book_wallert/models/book_model.dart';
 import 'package:http/http.dart' as http;
 import '../models/review_model.dart';
 
@@ -25,55 +25,38 @@ class ReviewForBookService {
 //   final String apiUrl = 'http://${ip}:3000/api/book/getBookId';
 
 //   Future<int> fetchId(BookModel book) async {
-//     final response = await http.get(Uri.parse('$apiUrl/${book.ISBN13}'));
+//     int? bookId;
+
+//     if (book.ISBN13.isNotEmpty) {
+//       bookId = await fetchIdUsingISBN(book.ISBN13);
+//     }
+
+//     if (bookId == null && book.ISBN10.isNotEmpty) {
+//       bookId = await fetchIdUsingISBN(book.ISBN10);
+//     }
+
+//     if (bookId != null) {
+//       return bookId;
+//     } else {
+//       throw Exception('Failed to get the book id using both ISBN13 and ISBN10');
+//     }
+//   }
+
+//   Future<int?> fetchIdUsingISBN(String ISBN) async {
+//     final response = await http.get(Uri.parse('$apiUrl/$ISBN'));
 //     print(response);
 
 //     if (response.statusCode == 200) {
-//       int bookId = jsonDecode(response.body);
-//       print(bookId);
-//       return bookId;
+//       List<dynamic> data = jsonDecode(response.body);
+//       if (data.isNotEmpty) {
+//         int bookId = data[0]['bookId'];
+//         print(bookId);
+//         return bookId;
+//       } else {
+//         throw Exception('No book ID found in the response');
+//       }
 //     } else {
 //       throw Exception('Failed to get the book id');
 //     }
 //   }
 // }
-
-class BookIdService {
-  final String apiUrl = 'http://${ip}:3000/api/book/getBookId';
-
-  Future<int> fetchId(BookModel book) async {
-    int? bookId;
-
-    if (book.ISBN13.isNotEmpty) {
-      bookId = await fetchIdUsingISBN(book.ISBN13);
-    }
-
-    if (bookId == null && book.ISBN10.isNotEmpty) {
-      bookId = await fetchIdUsingISBN(book.ISBN10);
-    }
-
-    if (bookId != null) {
-      return bookId;
-    } else {
-      throw Exception('Failed to get the book id using both ISBN13 and ISBN10');
-    }
-  }
-
-  Future<int?> fetchIdUsingISBN(String ISBN) async {
-    final response = await http.get(Uri.parse('$apiUrl/$ISBN'));
-    print(response);
-
-    if (response.statusCode == 200) {
-      List<dynamic> data = jsonDecode(response.body);
-      if (data.isNotEmpty) {
-        int bookId = data[0]['bookId'];
-        print(bookId);
-        return bookId;
-      } else {
-        throw Exception('No book ID found in the response');
-      }
-    } else {
-      throw Exception('Failed to get the book id');
-    }
-  }
-}
