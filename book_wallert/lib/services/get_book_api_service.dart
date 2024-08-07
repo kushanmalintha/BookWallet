3bd28b63f7574b321355219f9bc2967c065ea95d
kushanmalintha <e20243@eng.pdn.ac.lk>
2024-08-07 22:18:56 +0530

    fix errors

--- book_wallert/lib/services/get_book_api_service.dart
+++ book_wallert/lib/services/get_book_api_service.dart
@@ -11,18 +11,18 @@ class GetBookService {
     final response = await http.get(Uri.parse('$apiUrl/$bookId'));
     if (response.statusCode == 200) {
       List<dynamic> data = jsonDecode(response.body);
-      print(data);
-      print('title ${data[0]["title"].runtimeType}');
-      print('authore ${data[0]["author"].runtimeType}');
-      print('pages ${data[0]["pages"].runtimeType}');
-      print('gener ${data[0]["genre"].runtimeType}');
-      print('isbn10 ${data[0]["ISBN10"].runtimeType}');
-      print('isbn13 ${data[0]["ISBN13"].runtimeType}');
-      print('rating ${data[0]["totalRating"].runtimeType}');
-      print('pub date ${data[0]["publishedDate"].runtimeType}');
-      print('img ${data[0]["imageUrl"].runtimeType}');
-      print('des ${data[0]["description"].runtimeType}');
-      print('res ${data[0]["resource"].runtimeType}');
+      //print(data);
+      // print('title ${data[0]["title"].runtimeType}');
+      // print('authore ${data[0]["author"].runtimeType}');
+      // print('pages ${data[0]["pages"].runtimeType}');
+      // print('gener ${data[0]["genre"].runtimeType}');
+      // print('isbn10 ${data[0]["ISBN10"].runtimeType}');
+      // print('isbn13 ${data[0]["ISBN13"].runtimeType}');
+      // print('rating ${data[0]["totalRating"].runtimeType}');
+      // print('pub date ${data[0]["publishedDate"].runtimeType}');
+      // print('img ${data[0]["imageUrl"].runtimeType}');
+      // print('des ${data[0]["description"].runtimeType}');
+      // print('res ${data[0]["resource"].runtimeType}');
       return data.map((item) => BookModel.fromJson(item)).toList()[0];
     } else {
       throw Exception('Failed to load book');
