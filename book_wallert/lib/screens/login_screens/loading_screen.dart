56f4620ae6a2d140c9b10294a2552befc78256de
RaviAlpha <ravindupathirage12345@gmail.com>
2024-08-11 22:41:49 +0530

    removed the check connection

--- book_wallert/lib/screens/login_screens/loading_screen.dart
+++ book_wallert/lib/screens/login_screens/loading_screen.dart
@@ -1,5 +1,3 @@
-import 'dart:async';
-import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:book_wallert/colors.dart';
 import 'package:book_wallert/controllers/login_controller.dart';
 import 'package:book_wallert/services/auth_api_services.dart';
@@ -15,65 +13,9 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class LoadingScreenState extends State<LoadingScreen> {
-  late StreamSubscription<List<ConnectivityResult>> _connectivitySubscription;
-  bool _isConnected = false;
-  bool _hasLoggedIn = false;
-
-  @override
-  void initState() {
-    super.initState();
-    _checkConnectivityAndLogin();
-  }
-
-  Future<void> _checkConnectivityAndLogin() async {
-    _connectivitySubscription = Connectivity()
-        .onConnectivityChanged
-        .listen((List<ConnectivityResult> results) async {
-      // Check if there's at least one type of connectivity
-      bool hasConnection = results.any((result) =>
-          result == ConnectivityResult.mobile ||
-          result == ConnectivityResult.wifi);
-
-      if (hasConnection) {
-        _isConnected = true;
-        if (!_hasLoggedIn) {
-          await _attemptLogin();
-        }
-      } else {
-        _isConnected = false;
-        _navigateToMainScreen();
-      }
-    });
-  }
-
-  Future<void> _attemptLogin() async {
-    for (int i = 0; i < 3; i++) {
-      if (_isConnected) {
-        await LoginController.loginWithToken(context);
-        if (Navigator.of(context).canPop()) {
-          _hasLoggedIn = true;
-          return;
-        }
-      }
-      await Future.delayed(Duration(seconds: 2));
-    }
-    _navigateToMainScreen();
-  }
-
-  void _navigateToMainScreen() {
-    if (!_hasLoggedIn) {
-      Navigator.pushNamed(context, '/MainScreen');
-    }
-  }
-
-  @override
-  void dispose() {
-    _connectivitySubscription.cancel();
-    super.dispose();
-  }
-
   @override
   Widget build(BuildContext context) {
+    LoginController.loginWithToken(context);
     return Scaffold(
       backgroundColor: MyColors.bgColor,
       body: Center(
