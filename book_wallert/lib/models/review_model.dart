f18db0802490bd8a53ae7601f338e0ad0bcefa59
kushanmalintha <e20243@eng.pdn.ac.lk>
2024-08-16 22:18:37 +0530

    fix

--- book_wallert/lib/models/review_model.dart
+++ book_wallert/lib/models/review_model.dart
@@ -12,40 +12,39 @@ class ReviewModel {
   final int likesCount;
   final int commentsCount;
   final int sharesCount;
-  final int index;
 
-  ReviewModel(
-      {required this.reviewId,
-      required this.bookId,
-      required this.userId,
-      required this.imagePath,
-      required this.bookName,
-      required this.authorName,
-      required this.context,
-      required this.rating,
-      required this.date,
-      required this.reviwerName,
-      required this.likesCount,
-      required this.commentsCount,
-      required this.sharesCount,
-      required this.index});
+  ReviewModel({
+    required this.reviewId,
+    required this.bookId,
+    required this.userId,
+    required this.imagePath,
+    required this.bookName,
+    required this.authorName,
+    required this.context,
+    required this.rating,
+    required this.date,
+    required this.reviwerName,
+    required this.likesCount,
+    required this.commentsCount,
+    required this.sharesCount,
+  });
 
   factory ReviewModel.fromJson(Map<String, dynamic> json) {
     return ReviewModel(
-        reviewId: json['reviewId'],
-        bookId: json['bookId'],
-        userId: json['userId'],
-        imagePath: json['imagePath'],
-        bookName: json['bookName'],
-        authorName: json['authorName'],
-        context: json['context'],
-        rating: json['rating'].toDouble(),
-        date: json['date'],
-        reviwerName: json['reviwerName'],
-        likesCount: json['likesCount'], // Nullable
-        commentsCount: json['commentsCount'], // Nullable
-        sharesCount: json['sharesCount'], // Nullable
-        index: json['index']);
+      reviewId: json['reviewId'],
+      bookId: json['bookId'],
+      userId: json['userId'],
+      imagePath: json['imagePath'],
+      bookName: json['bookName'],
+      authorName: json['authorName'],
+      context: json['context'],
+      rating: json['rating'].toDouble(),
+      date: json['date'],
+      reviwerName: json['reviwerName'],
+      likesCount: json['likesCount'], // Nullable
+      commentsCount: json['commentsCount'], // Nullable
+      sharesCount: json['sharesCount'], // Nullable
+    );
   }
 
   Map<String, dynamic> toJson() {
@@ -63,7 +62,6 @@ class ReviewModel {
       'likesCount': likesCount, // Nullable
       'commentsCount': commentsCount, // Nullable
       'sharesCount': sharesCount, // Nullable
-      'index': index
     };
   }
 }
