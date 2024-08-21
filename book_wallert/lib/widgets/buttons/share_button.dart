0e955d510ea2ae35d281d05cdeaa6c9fe48374df
RaviAlpha <ravindupathirage12345@gmail.com>
2024-08-21 22:21:17 +0530

    added QR scanning to search ISBN

--- book_wallert/lib/widgets/buttons/share_button.dart
+++ book_wallert/lib/widgets/buttons/share_button.dart
@@ -1,15 +1,17 @@
+import 'package:book_wallert/functions/global_navigator_functions.dart';
 import 'package:book_wallert/functions/global_user_provider.dart';
+import 'package:book_wallert/screens/review_screens/review_screen_body.dart';
 import 'package:flutter/material.dart';
-import 'package:book_wallert/services/share_service.dart'; // Assuming you have a ShareService
+import 'package:book_wallert/services/share_service.dart';
 import 'package:book_wallert/models/review_model.dart';
 import 'package:book_wallert/colors.dart';
 
 class ShareButton extends StatefulWidget {
   final ReviewModel review;
   final IconData icon;
-  int sharesCount;
+  final int sharesCount;
 
-  ShareButton({
+  const ShareButton({
     Key? key,
     required this.review,
     required this.icon,
@@ -20,45 +22,68 @@ class ShareButton extends StatefulWidget {
   _ShareButtonState createState() => _ShareButtonState();
 }
 
-class _ShareButtonState extends State<ShareButton> {
-  late ShareService _shareService;
+class _ShareButtonState extends State<ShareButton>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _controller;
   bool isShared = false;
+  int _shareCount = 0;
+  final ShareService _shareService = ShareService();
 
   @override
   void initState() {
     super.initState();
-    _shareService = ShareService();
-    _checkIfShared();
+    _controller = AnimationController(
+        duration: const Duration(milliseconds: 200), vsync: this, value: 1.0);
+
+    _shareCount = widget.sharesCount;
+    _checkIfShared(); // Check if the user has shared the review
   }
 
   Future<void> _checkIfShared() async {
     try {
-      bool shared = await _shareService.checkIfShared(widget.review.reviewId, globalUser!.userId);
+      final userId = globalUser?.userId;
+      if (userId == null) {
+        // Handle the case where the user is not logged in or userId is not available
+        print('User is not logged in');
+        return;
+      }
+
+      final shared =
+          await _shareService.checkIfShared(widget.review.reviewId, userId);
       setState(() {
         isShared = shared;
       });
     } catch (e) {
-      print('Error checking if review is shared: $e');
+      print('Failed to check if shared: $e');
     }
   }
 
-  void _handleShare() async {
+  Future<void> _handleShare() async {
     try {
+      final userId = globalUser?.userId;
+      if (userId == null) {
+        // Handle the case where the user is not logged in or userId is not available
+        print('User is not logged in');
+        return;
+      }
+
       if (isShared) {
-        await _shareService.shareReview(widget.review.reviewId, globalUser!.userId);
+        await _shareService.shareReview(widget.review.reviewId, userId);
         setState(() {
-          widget.sharesCount--;
+          _shareCount--;
           isShared = false;
         });
       } else {
-        await _shareService.shareReview(widget.review.reviewId, globalUser!.userId);
+        await _shareService.shareReview(widget.review.reviewId, userId);
         setState(() {
-          widget.sharesCount++;
+          _shareCount++;
           isShared = true;
         });
       }
+      // Trigger animation without affecting button state
+      _controller.forward().then((_) => _controller.reverse());
     } catch (e) {
-      print('Error sharing/unsharing review: $e');
+      print('Failed to toggle share: $e');
     }
   }
 
@@ -66,21 +91,41 @@ class _ShareButtonState extends State<ShareButton> {
   Widget build(BuildContext context) {
     return Row(
       children: [
-        IconButton(
-          icon: Icon(
-            widget.icon,
-            color: isShared ? MyColors.selectedItemColor : MyColors.nonSelectedItemColor,
+        GestureDetector(
+          onTap: _handleShare,
+          child: ScaleTransition(
+            scale: Tween(begin: 0.95, end: 1.05).animate(
+                CurvedAnimation(parent: _controller, curve: Curves.easeOut)),
+            child: Icon(
+              widget.icon,
+              size: 21,
+              color: isShared
+                  ? MyColors.selectedItemColor
+                  : MyColors.nonSelectedItemColor,
+            ),
           ),
-          onPressed: _handleShare,
         ),
-        Text(
-          '${widget.sharesCount}', // Display the share count
-          style: const TextStyle(
-            color: MyColors.text2Color,
-            fontSize: 14,
+        TextButton(
+          onPressed: () {
+            screenChange(
+                context,
+                ReviewScreenBody(
+                  review: widget.review,
+                  index: 2,
+                ));
+          }, // Optionally, trigger the share on text tap as well
+          child: Text(
+            '$_shareCount', // Display the share count
+            style: const TextStyle(color: MyColors.text2Color, fontSize: 14),
           ),
         ),
       ],
     );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 }
