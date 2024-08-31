3f4739339360c26eaf761bb376044c7b55510a06
RaviAlpha <ravindupathirage12345@gmail.com>
2024-08-31 13:02:19 +0530

    Merge branch 'main' into barcodebookadd

--- book_wallert/macos/Flutter/GeneratedPluginRegistrant.swift
+++ book_wallert/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,12 +6,14 @@ import FlutterMacOS
 import Foundation
 
 import file_selector_macos
+import google_sign_in_ios
 import path_provider_foundation
 import pdf_render
 import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
+  FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SwiftPdfRenderPlugin.register(with: registry.registrar(forPlugin: "SwiftPdfRenderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
