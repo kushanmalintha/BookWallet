8bce184030fa76d2b4f4169b9ed19ef4857e8776
RaviAlpha <ravindupathirage12345@gmail.com>
2024-09-15 00:35:04 +0530

    renamed

--- book_wallert/lib/screens/test_screens/screen6/screen6.dart
+++ book_wallert/lib/screens/test_screens/screen6/screen6.dart
@@ -1,7 +1,7 @@
+import 'package:book_wallert/controllers/group_controller.dart';
 import 'package:book_wallert/widgets/buttons/custom_button1.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:book_wallert/controllers/groupController.dart';
 import 'package:book_wallert/colors.dart';
 
 class Screen6 extends StatelessWidget {
